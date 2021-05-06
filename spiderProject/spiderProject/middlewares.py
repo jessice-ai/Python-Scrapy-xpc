ec1cd702f54709081ca3c466e7842483cbd7e60e
Jessice <jessicesun@gmail.com>
2021-05-06 13:37:16 +0800

    实现代理请求远程网站

--- spiderProject/spiderProject/middlewares.py
+++ spiderProject/spiderProject/middlewares.py
@@ -12,6 +12,7 @@ from scrapy import signals
 from itemadapter import is_item, ItemAdapter
 from scrapy.core.downloader.handlers.http11 import TunnelError
 from scrapy.exceptions import NotConfigured
+from scrapy.extensions.closespider import CloseSpider
 from scrapy.utils.response import response_status_message
 from twisted.internet import defer
 from twisted.internet.error import DNSLookupError, TCPTimedOutError, ConnectionLost, ConnectError, ConnectionDone
@@ -113,14 +114,22 @@ class SpiderprojectDownloaderMiddleware:
 
 from urllib.parse import urlparse
 from scrapy.exceptions import IgnoreRequest
+import random
+from scrapy.core.downloader.handlers.http11 import TunnelError
+from scrapy.exceptions import NotConfigured
+from twisted.internet import defer
+from twisted.internet.error import DNSLookupError, TCPTimedOutError, ConnectionLost, ConnectError
+from twisted.web._newclient import ResponseFailed
+from scrapy.exceptions import IgnoreRequest
+
 #自定义中间件
 class RandomProxyMiddlewate(object):
 
-    sun_proxy = "" #代理属性 HTTP_PROXY 或  HTTPS_PROXY
-
     #第二部,初始化类属性
     def __init__(self,settings):
-
+        self.SUN_RETRY_TIMES = 0; #重试次数
+        self.SUN_RETRY_TIMES_RESPONSE = 0;  # 重试次数
+        self.RETRY_TIMES =  settings.getint('RETRY_TIMES')
         # 这里涉及到了settings.py文件中的几个量
         # RETRY_ENABLED: 用于开启中间件，默认为TRUE
         # RETRY_TIMES: 重试次数, 默认为2
@@ -129,7 +138,7 @@ class RandomProxyMiddlewate(object):
 
         if not settings.getbool('RETRY_ENABLED'):
             raise NotConfigured
-        self.max_retry_times = settings.getint('RETRY_TIMES')
+        # self.max_retry_times = settings.getint('RETRY_TIMES')
         self.retry_http_codes = set(int(x) for x in settings.getlist('RETRY_HTTP_CODES'))
         self.priority_adjust = settings.getint('RETRY_PRIORITY_ADJUST')
 
@@ -149,26 +158,20 @@ class RandomProxyMiddlewate(object):
         print('1、创建中间件对象')
         return cls(crawler.settings)  #使用cls参数,执行 init 函数
 
-
     #第三步,为每一个request对象,设置一个IP代理
     def process_request(self,request,spider):
         # 这里的 random 选择 random 导入
         sx = random.choice(self.PROXYS)
-        scheme = urlparse(sx).scheme
-        netloc = urlparse(sx).netloc
-        pre_proxy = ''
-        if scheme == 'http':
-            self.pre_proxy = 'HTTP_PROXY'
-        elif scheme == 'https':
-            self.pre_proxy = 'HTTPS_PROXY'
+        self.scheme = urlparse(sx).scheme
+        # netloc = urlparse(sx).netloc
 
-        if not request.meta.get(self.pre_proxy): #如果请求中没有proxy这个属性，则给设置一个随机的代理
-            request.meta[self.pre_proxy] = netloc
+        if not request.meta.get(self.scheme): #如果请求中没有proxy这个属性，则给设置一个随机的代理
+            request.meta[self.scheme] = sx # 结构 equest.meta['http'] = 'http://193.149.225.72:80'
             # print('3、为每一个request对象,设置一个随机IP代理')
             print('-' * 80)
             print(' ' * 80)
             print(' ' * 80)
-            print("3、Request对象 request.meta[%s] = %s" % (self.pre_proxy,sx))
+            print("3、Request对象 设置代理 request.meta[%s] = %s " % (self.scheme,sx))
             print('-' * 80)
             print(' ' * 80)
         pass
@@ -178,8 +181,8 @@ class RandomProxyMiddlewate(object):
     # 2、代理可用后,调用,(检查对方服务器是否禁用了这个代理)
     # 注意:process_response 与 process_exception 会执行一个
     def process_response(self,request,response,spider):
-        # print(dir(request.meta))
-        curl_proxy = request.meta.get(self.pre_proxy)
+        #print(dir(response))
+        curl_proxy = request.meta.get(self.scheme)
         #if response.status in (401,403):
         print('-' * 80)
         print(' ' * 80)
@@ -187,26 +190,30 @@ class RandomProxyMiddlewate(object):
         #判断代理IP是否被对方服务器封禁
         if response.status != 200:
             #给相应的代理IP累计失败次数
-            self.state[curl_proxy] += 1
+            self.SUN_RETRY_TIMES_RESPONSE += 1
             # print(self.state)
             print('5、服务器 -> 错误码 (%s) %s次 代理 %s' % (response.status, self.state[curl_proxy],curl_proxy))
             #当某个IP的失败次数累计到一定量
-            if self.state[curl_proxy] >= self.max_retry_times:
+            if self.SUN_RETRY_TIMES_RESPONSE == self.RETRY_TIMES:
                 #可以认为该IP已经被对方服务器封禁,从代理池中删除该IP
                 print('5、服务器 -> 错误码 (%s) ,拒绝IP 超%s次,已删除 代理 ' % (response.status, self.max_failed,curl_proxy))
                 self.PROXYS.remove(curl_proxy)
+                del request.meta[self.scheme]
+
                 # 如果代理池中没有可用的代理,则抛出异常,请求就不再往下走了，到此为止
                 if len(self.PROXYS) < 1:
+                    print('强制停止,代理数组中没有数据了')
                     raise IgnoreRequest
-                del request.meta[self.pre_proxy]
                 #注意
                 # 1、返回request 作用: 将该请求,重新安排调度下载
                 # 2、返回request重试时，在爬虫Spider里一定要用yield返回，否则不会重发request
-                reason = response_status_message(response.status)
+                # reason = response_status_message(response.status)
                 # return self._retry(request, reason, spider) or response
-                return request
+
             else:
                 print('5、服务器 -> 错误码 (%s) 拒绝IP（%s） 次数 %s ' % (response.status,curl_proxy,self.state[curl_proxy]))
+
+            return request
         else:
             print('5、服务器 -> 返回码 (%s) ' % response.status)
         return response
@@ -215,7 +222,8 @@ class RandomProxyMiddlewate(object):
     # 2、检测代理是否有异常
     # 注意:process_response 与 process_exception 会执行一个
     def process_exception(self,request,exception,spider):
-        curl_proxy = request.meta.get(self.pre_proxy)
+        # print(dir(exception))
+        curl_proxy = request.meta.get(self.scheme)
         # twisted.internet.error.ConnectionRefusedError 异常问题, twisted.internet.error.TimeoutError
         from twisted.internet.error import ConnectionRefusedError,TimeoutError,ConnectionDone
         from scrapy.spidermiddlewares import httperror
@@ -228,6 +236,9 @@ class RandomProxyMiddlewate(object):
                                ConnectionLost, TCPTimedOutError, ResponseFailed,
                                IOError, TunnelError)
         if curl_proxy and (isinstance(exception,EXCEPTIONS_TO_RETRY)):
+            self.SUN_RETRY_TIMES +=1
+            # print(self.SUN_RETRY_TIMES)
+            # print(self.SUN_RETRY_TIMES)
             print('4、请求失败,异常:(%s) 代理 %s' % (exception,curl_proxy))
             print('-' * 80)
             if curl_proxy in self.PROXYS:
@@ -236,10 +247,23 @@ class RandomProxyMiddlewate(object):
                 #如果代理池中没有可用的代理,则抛出异常,请求就不再往下走了，到此为止
                 if len(self.PROXYS) < 1:
                     raise IgnoreRequest
-                del request.meta[self.pre_proxy]
-                #这里要注释掉,如果代理有问题,则会自动切换其他代理,如果去掉注释不会自动重试,配合设置中的参数 RETRY_TIMES = 50 重试50次,直到成功,则停止
-                # return request
-                print('*'*100)
+                del request.meta[self.scheme]
+        print(''*30)
+        print('*' * 100)
+        #验证是否超过最高重试次数
+        if self.SUN_RETRY_TIMES == self.RETRY_TIMES:
+            print('强制退出,重试超过最大（%s）次' % self.RETRY_TIMES)
+            print('*' * 100)
+        else:
+            # return request 标识: 如果代理IP有问题,则会自动切换重新执行
+            # process_request 获取新的IP地址,设置中的参数 RETRY_TIMES = 15 表示最多重试16次执行 process_request获取新的代理IP ,然后不断提示放弃重试
+            return request
+
+
+
+
+
+
 
 
 
--- spiderProject/spiderProject/spiders/sun_xpc.py
+++ spiderProject/spiderProject/spiders/sun_xpc.py
@@ -1,32 +1,35 @@
-
-
 import scrapy
-from scrapy import Request
 from fake_useragent import UserAgent
-
+from urllib.parse import urlparse
 
 class SunXpcSpider(scrapy.Spider):
     name = 'sun_xpc'
     allowed_domains = ['xinpianchang.com']
-    start_urls = ['https://www.xinpianchang.com/channel/index/type-/sort-like/duration_type-0/resolution_type-/page-1']
+    start_urls = ['https://www.xinpianchang.com/channel/index/type-/sort-like/duration_type-0/resolution_type-/page-2']
 
     headers = {
         'User-Agent': UserAgent().chrome,
+        "Content-Type": "application/json;charset=utf-8",
+        # Host 使用第一个网址中的全域名,如果手动写，注意 aaa.com 与 www.aaa.com 解析后的IP地址不同情况
+        # Host 是 HTTP / 1.1 必须包含参数,作用:指定用户要访问的域名
+        "Host": "%s:80" % urlparse(start_urls[0]).netloc,
     }
+
     def start_requests(self):
         for url in self.start_urls:
             # 说明 dont_filter=True Scrapy内置了重复过滤功能,设置为True表示,不过滤重复请求
-            yield scrapy.Request(url, headers=self.headers,dont_filter=True)
+            yield scrapy.Request(url,callback=self.parse, headers=self.headers,dont_filter=True)
 
     def parse(self, response):
+
         list = response.xpath('//ul[@class="video-list"]/li/@data-articleid').extract() # extract() 从一个对象中得到列表[]
         for item in  list:
             url = 'https://www.xinpianchang.com/a%s?from=ArticleList'
-            #yield response.follow()
-            print(url % item)
-            yield response.follow(url % item,self.parse_post)
-        pass
+            yield response.follow(url % item,self.parse_post,headers=self.headers,dont_filter=True)
+
 
     def parse_post(self,response):
-        title = response.xpath('//h3[contains(@class,"title fs_26 fw_600 c_b_3 short")]/text()').extract()
-        print(title)
+        post = {}
+        post['titile'] = response.xpath('//h3[contains(@class,"title fs_26 fw_600 c_b_3 short")]/text()').extract()
+        # yield post
+        print(post)
